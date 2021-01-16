654c6081cf32799263b951f8c7e2214faaae8575
devilzeros <42342644+devilzeros@users.noreply.github.com>
2021-01-16 17:00:30 +0700

    est

--- pages/_app.js
+++ pages/_app.js
@@ -40,6 +40,7 @@ export default function MyApp({ Component, pageProps }) {
       <script src="assets/js/TweenMax.js"></script>
       <script src="assets/js/mousemoveparallax.js"></script>
       <script src="assets/js/main.js"></script>
+      <script src="js/jquery-1.11.3.min.js"></script>
     </>
   );
 }
