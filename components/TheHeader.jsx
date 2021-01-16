9937c3e532ffb6e723d5438213ff69813f7240fb
devilzeros <42342644+devilzeros@users.noreply.github.com>
2021-01-16 18:13:13 +0700

    est

--- components/TheHeader.jsx
+++ components/TheHeader.jsx
@@ -4,29 +4,30 @@ export default function TheHeader() {
     return (
         <>
             <header className="header">
-                <Navbar bg="black" expand="lg" className="container">
-                    <Navbar.Brand href="#home">
-                        <img
-                            src="assets/images/logoicon1.png"
-                            width="100"
-                            height="100"
-                            alt=""
-                            style={{ position: "relative", objectFit: "contain" }}
-                        />
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ml-auto">
-                            <Nav.Link href="/home">หน้าแรก</Nav.Link>
-                            <Nav.Link href="/bacara">บาคาร่า</Nav.Link>
-                            <Nav.Link href="/shootfish">ยิงปลา</Nav.Link>
-                            <Nav.Link href="/slot">สล็อต</Nav.Link>
-                            <Nav.Link href="/sport">แทงบอล</Nav.Link>
-                            <Nav.Link href="/login">เข้าสู่เกมส์</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
+                <Navbar bg="black" expand="lg" >
+                    <div className="container">
+                        <Navbar.Brand href="#home">
+                            <img
+                                src="assets/images/logoicon1.png"
+                                width="100"
+                                height="100"
+                                alt=""
+                                style={{ position: "relative", objectFit: "contain" }}
+                            />
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="ml-auto">
+                                <Nav.Link href="/home">หน้าแรก</Nav.Link>
+                                <Nav.Link href="/bacara">บาคาร่า</Nav.Link>
+                                <Nav.Link href="/shootfish">ยิงปลา</Nav.Link>
+                                <Nav.Link href="/slot">สล็อต</Nav.Link>
+                                <Nav.Link href="/sport">แทงบอล</Nav.Link>
+                                <Nav.Link href="/login">เข้าสู่เกมส์</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </div>
                 </Navbar>
-                {/* <!--Main-Menu Area Start--> */}
             </header>
         </>
     );
