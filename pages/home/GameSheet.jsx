de175e5579f5a85e0b4b6cb291dedd9318e5836c
devilzeros <42342644+devilzeros@users.noreply.github.com>
2021-01-30 18:13:44 +0700

    est

--- pages/home/GameSheet.jsx
+++ pages/home/GameSheet.jsx
@@ -8,162 +8,137 @@ export default function GameSheet() {
 					<h3 className="text-neno-titel">เว็บคาสิโน จากค่ายชั้นนำ</h3>
 				</div>
 				<div className="box-game">
-					<Link href="/login">
-						<a className="list-game">
-							<div >
-								<img src="https://sv1.picz.in.th/images/2021/01/25/lHONbt.png" alt="Virtual,Sports,Virtual+Sports,แทงบอลออนไลน์" />{" "}
-								<p className="m-0" style={{ color: "beige" }}>แทงบอลออนไลน์</p>
-							</div>
-						</a>
-					</Link>
-					<Link href="/login">
-						<a>
-							<div className="list-game">
-								{" "}
-								<img src="https://sv1.picz.in.th/images/2021/01/25/lHEJeb.png" alt="Ebet,บาคารา" />{" "}
-								<p className="m-0" style={{ color: "beige" }}>บาคาราออนไลน์</p>
-
-							</div>
-						</a>
-					</Link>
-				</div>
-				<div className="row text-center " style={{ color: "beige" }}>
-					<div className="col-lg-3 col-md-4 col-xs-4 col-sm-5 p-md-3 check">
+					{/* <div className="list-game">
 						<Link href="/login">
 							<a>
-								<div className="card-img-home">
-									{" "}
-									<img src="https://sv1.picz.in.th/images/2021/01/25/lHEJeb.png" alt="Ebet,บาคารา" />{" "}
-								</div>
-								<p className="m-0" style={{ color: "beige" }}>บาคาราออนไลน์</p>
-							</a>
-						</Link>
-					</div>
-					<div className="col-lg-3 col-md-4 col-xs-4 col-sm-5 p-md-3 check">
-						<Link href="/login">
-							<a>
-								<div className="card-img-home">
-									{" "}
-									<img src="https://sv1.picz.in.th/images/2021/01/25/lHEmOa.png" alt="Tip,เกมสล๊อต" />{" "}
-								</div>
-								<p className="m-0" style={{ color: "beige" }}>เกมสล๊อต</p>
-							</a>
-						</Link>
-					</div>
-					<div className="col-lg-3 col-md-4 col-xs-4 col-sm-5 p-md-3 check">
-						<Link href="/login">
-							<a>
-								<div className="card-img-home">
-									{" "}
-									<img src="https://sv1.picz.in.th/images/2021/01/25/lHEyaq.png" alt="Joker,สล๊อต,ยิงปลา" />{" "}
-								</div>
-								<p className="m-0" style={{ color: "beige" }}>สล๊อต และยิงปลา</p>
-							</a>
-						</Link>
-					</div>
-					<div className="col-lg-3 col-md-4 col-xs-4 col-sm-5 p-md-3 check">
-						<Link href="/login">
-							<a>
-								<div className="card-img-home">
-									{" "}
-									<img src="https://sv1.picz.in.th/images/2021/01/25/lHEI4z.png" alt="Hi-Lo,HiLo,ไฮโล" />{" "}
-								</div>
-								<p className="m-0" style={{ color: "beige" }}>ไฮโล</p>
-							</a>
-						</Link>
-					</div>
-					<div className="col-lg-3 col-md-4 col-xs-4 col-sm-5 p-md-3 check">
-						<Link href="/login">
-							<a>
-								<div className="card-img-home">
-									{" "}
-									<img src="https://sv1.picz.in.th/images/2021/01/25/lHEMx8.png" alt="MuayStep,มวยสเต็ป" />{" "}
-								</div>
-								<p className="m-0" style={{ color: "beige" }}>มวยสเต็ป</p>
-							</a>
-						</Link>
-					</div>
-					<div className="col-lg-3 col-md-4 col-xs-4 col-sm-5 p-md-3 check">
-						<Link href="/login">
-							<a>
-								<div className="card-img-home">
-									{" "}
-									<img src="https://sv1.picz.in.th/images/2021/01/25/lHOWnR.png" alt="Pokdeng,ป๊อกเด้ง" />{" "}
-								</div>
-								<p className="m-0" style={{ color: "beige" }}>ป๊อกเด้ง</p>
-							</a>
-						</Link>
-					</div>
-					<div className="col-lg-3 col-md-4 col-xs-4 col-sm-5 p-md-3 check">
-						<Link href="/login">
-							<a>
-								<div className="card-img-home">
-									{" "}
-									<img src="https://sv1.picz.in.th/images/2021/01/25/lHO0d0.png" alt="SexyBaccara,เว็กซี่บาคารา" />{" "}
-								</div>
-								<p className="m-0" style={{ color: "beige" }}>เว็กซี่บาคารา</p>
-							</a>
-						</Link>
-					</div>
-					<div className="col-lg-3 col-md-4 col-xs-4 col-sm-5 p-md-3 check">
-						<Link href="/login">
-							<a>
-								<div className="card-img-home">
-									{" "}
-									<img src="https://sv1.picz.in.th/images/2021/01/25/lHO7Ku.png" alt="Slotto,หวยออนไลน์" />{" "}
-								</div>
-								<p className="m-0" style={{ color: "beige" }}>หวยออนไลน์</p>
-							</a>
-						</Link>
-					</div>
-					<div className="col-lg-3 col-md-4 col-xs-4 col-sm-5 p-md-3 check">
-						<Link href="/login">
-							<a>
-								<div className="card-img-home">
-									{" "}
-									<img src="https://sv1.picz.in.th/images/2021/01/25/lHOSPZ.png" alt="Padeg,เกมสล๊อต" />{" "}
-								</div>
-								<p className="m-0" style={{ color: "beige" }}>เกมสล๊อต</p>
-							</a>
-						</Link>
-					</div>
-					<div className="col-lg-3 col-md-4 col-xs-4 col-sm-5 p-md-3 check">
-						<Link href="/login">
-							<a>
-								<div className="card-img-home">
-									{" "}
-									<img src="https://sv1.picz.in.th/images/2021/01/25/lHOYeI.png" alt="Sv 388,แทงไก่ชนออนไลน์" />{" "}
-								</div>
-								<p className="m-0" style={{ color: "beige" }}>แทงไก่ชนออนไลน์</p>
-							</a>
-						</Link>
-					</div>
-					<div className="col-lg-3 col-md-4 col-xs-4 col-sm-5 p-md-3 check">
-						<Link href="/login">
-							<a>
-								<div className="card-img-home">
-									{" "}
-									<img src="https://sv1.picz.in.th/images/2021/01/25/lHO43P.png" alt="Ufa-Slot,เกมสล๊อต" />{" "}
-								</div>
-								<p className="m-0" style={{ color: "beige" }}>เกมสล๊อต</p>
-							</a>
-						</Link>
-					</div>
-					<div className="col-lg-3 col-md-4 col-xs-4 col-sm-5 p-md-3 check">
-						<Link href="/login">
-							<a>
-								<div className="card-img-home">
-									{" "}
-									<img src="https://sv1.picz.in.th/images/2021/01/25/lHONbt.png" alt="Virtual,Sports,Virtual+Sports,แทงบอลออนไลน์" />{" "}
-								</div>
+								<img src="https://sv1.picz.in.th/images/2021/01/25/lHEJeb.png" alt="Virtual,Sports,Virtual+Sports,แทงบอลออนไลน์" />
 								<p className="m-0" style={{ color: "beige" }}>แทงบอลออนไลน์</p>
 							</a>
 						</Link>
+</div>
+					</div> */}
+					<div className="m-auto">
+						<div className="list-game">
+							<Link href="/login">
+								<a>
+									<img src="https://sv1.picz.in.th/images/2021/01/25/lHEJeb.png" alt="Ebet,บาคารา" />
+									<p className="m-0" style={{ color: "beige" }}>บาคาราออนไลน์</p>
+								</a>
+							</Link>
+						</div>
+					</div>
+					<div className="m-auto">
+						<div className="list-game">
+							<Link href="/login">
+								<a>
+									<img src="https://sv1.picz.in.th/images/2021/01/25/lHEmOa.png" alt="Tip,เกมสล๊อต" />
+									<p className="m-0" style={{ color: "beige" }}>เกมสล๊อต</p>
+								</a>
+							</Link>
+						</div>
+					</div>
+					<div className="m-auto">
+						<div className="list-game">
+							<Link href="/login">
+								<a>
+									<img src="https://sv1.picz.in.th/images/2021/01/25/lHEyaq.png" alt="Joker,สล๊อต,ยิงปลา" />
+									<p className="m-0" style={{ color: "beige" }}>สล๊อต และยิงปลา</p>
+								</a>
+							</Link>
+						</div>
+					</div>
+					<div className="m-auto">
+						<div className="list-game">
+							<Link href="/login">
+								<a>
+									<img src="https://sv1.picz.in.th/images/2021/01/25/lHEI4z.png" alt="Hi-Lo,HiLo,ไฮโล" />
+									<p className="m-0" style={{ color: "beige" }}>ไฮโล</p>
+								</a>
+							</Link>
+						</div>
+					</div>
+					<div className="m-auto">
+						<div className="list-game">
+							<Link href="/login">
+								<a>
+									<img src="https://sv1.picz.in.th/images/2021/01/25/lHEMx8.png" alt="MuayStep,มวยสเต็ป" />
+									<p className="m-0" style={{ color: "beige" }}>มวยสเต็ป</p>
+								</a>
+							</Link>
+						</div>
+					</div>
+					<div className="m-auto">
+						<div className="list-game">
+							<Link href="/login">
+								<a>
+									<img src="https://sv1.picz.in.th/images/2021/01/25/lHOWnR.png" alt="Pokdeng,ป๊อกเด้ง" />
+									<p className="m-0" style={{ color: "beige" }}>ป๊อกเด้ง</p>
+								</a>
+							</Link>
+						</div>
+					</div>
+					<div className="m-auto">
+						<div className="list-game">
+							<Link href="/login">
+								<a>
+									<img src="https://sv1.picz.in.th/images/2021/01/25/lHO0d0.png" alt="SexyBaccara,เว็กซี่บาคารา" />
+									<p className="m-0" style={{ color: "beige" }}>เว็กซี่บาคารา</p>
+								</a>
+							</Link>
+						</div>
+					</div>
+					<div className="m-auto">
+						<div className="list-game">
+							<Link href="/login">
+								<a>
+									<img src="https://sv1.picz.in.th/images/2021/01/25/lHO7Ku.png" alt="Slotto,หวยออนไลน์" />
+									<p className="m-0" style={{ color: "beige" }}>หวยออนไลน์</p>
+								</a>
+							</Link>
+						</div>
+					</div>
+					<div className="m-auto">
+						<div className="list-game">
+							<Link href="/login">
+								<a>
+									<img src="https://sv1.picz.in.th/images/2021/01/25/lHOSPZ.png" alt="Padeg,เกมสล๊อต" />
+									<p className="m-0" style={{ color: "beige" }}>เกมสล๊อต</p>
+								</a>
+							</Link>
+						</div>
+					</div>
+					<div className="m-auto">
+						<div className="list-game">
+							<Link href="/login">
+								<a>
+									<img src="https://sv1.picz.in.th/images/2021/01/25/lHOYeI.png" alt="Sv 388,แทงไก่ชนออนไลน์" />
+									<p className="m-0" style={{ color: "beige" }}>แทงไก่ชนออนไลน์</p>
+								</a>
+							</Link>
+						</div>
+					</div>
+					<div className="m-auto">
+						<div className="list-game">
+							<Link href="/login">
+								<a>
+									<img src="https://sv1.picz.in.th/images/2021/01/25/lHO43P.png" alt="Ufa-Slot,เกมสล๊อต" />
+									<p className="m-0" style={{ color: "beige" }}>เกมสล๊อต</p>
+								</a>
+							</Link>
+						</div>
+					</div>
+					<div className="m-auto">
+						<div className="list-game">
+							<Link href="/login">
+								<a>
+									<img src="https://sv1.picz.in.th/images/2021/01/25/lHONbt.png" alt="Virtual,Sports,Virtual+Sports,แทงบอลออนไลน์" />
+									<p className="m-0" style={{ color: "beige" }}>แทงบอลออนไลน์</p>
+								</a>
+							</Link>
+						</div>
 					</div>
 				</div>
-
 			</div>
-
 		</>
 	);
 }
