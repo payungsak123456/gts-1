131ebe3e6c0280df14c48f961e86478a70fa1d05
Gtsgold <78080112+gtsgolde@users.noreply.github.com>
2021-03-25 18:39:52 +0700

    okนะ

--- components/TheMobileMenu.jsx
+++ components/TheMobileMenu.jsx
@@ -24,7 +24,31 @@ export default function TheMobileMenu() {
 									src="https://sv1.picz.in.th/images/2021/01/29/lJmVPS.png"
 								/>
 							</noscript>
-							<span className="text-contact">สมัครอัตโนมัติ</span>
+							<span className="text-contact">สมัคร Gclub</span>
+						</a>
+					</Link>
+				</div>
+				<div className="o-contact">
+					<Link href="https://customer.gtsgold168.com/register?agent=ufa">
+						<a>
+							<img
+								className="contact-icon lazyloaded"
+								alt="follower ufadeal"
+								width="24px"
+								height="24px"
+								src="https://sv1.picz.in.th/images/2021/01/29/lJmVPS.png"
+								data-ll-status="loaded"
+							/>
+							<noscript>
+								<img
+									className="contact-icon"
+									alt="follower ufadeal"
+									width="24px"
+									height="24px"
+									src="https://sv1.picz.in.th/images/2021/01/29/lJmoAP.png"
+								/>
+							</noscript>
+							<span className="text-contact contact-info">สมัคร Ufa</span>
 						</a>
 					</Link>
 				</div>
@@ -51,30 +75,6 @@ export default function TheMobileMenu() {
 								/>
 							</noscript>
 							<span className="text-contact">เข้าสู่ระบบ</span>
-						</a>
-					</Link>
-				</div>
-				<div className="o-contact">
-					<Link href="https://lin.ee/SGc236G">
-						<a>
-							<img
-								className="contact-icon lazyloaded"
-								alt="follower ufadeal"
-								width="24px"
-								height="24px"
-								src="https://sv1.picz.in.th/images/2021/01/29/lJmoAP.png"
-								data-ll-status="loaded"
-							/>
-							<noscript>
-								<img
-									className="contact-icon"
-									alt="follower ufadeal"
-									width="24px"
-									height="24px"
-									src="https://sv1.picz.in.th/images/2021/01/29/lJmoAP.png"
-								/>
-							</noscript>
-							<span className="text-contact contact-info">สมัครทาง line</span>
 						</a>
 					</Link>
 				</div>
