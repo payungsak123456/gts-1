8468360cdecf239d53306940704510a6a63ad6b2
Gtsgold <78080112+gtsgolde@users.noreply.github.com>
2021-03-31 00:20:43 +0700

    pi

--- components/ThePopup.jsx
+++ components/ThePopup.jsx
@@ -10,7 +10,7 @@ export default function ThePopup() {
                 <Modal.Header closeButton>
                 </Modal.Header>
                 <Modal.Body>
-                <a href="https://lin.ee/SGc236G">
+                <a href="https://customer.gtsgold168.com/register?agent=ufa">
                     <Image src="https://sv1.picz.in.th/images/2021/03/25/DuKqw1.jpg"alt="คาสิโนออนไลน์,แทงบอล,ยิงปลา" className="border border-white rounded" />
                 </a>   
                 </Modal.Body>
